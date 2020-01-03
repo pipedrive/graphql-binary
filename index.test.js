a425e6b6ebe5ed3b7566b708b34e9c40894f2c59
Artjom Kurapov <artkurapov@gmail.com>
2020-01-03 15:27:22 +0200

    moved schema & query to files

--- index.test.js
+++ index.test.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const { assert } = require('chai');
 const { parse, print } = require('graphql');
 const compress = require('graphql-query-compress');
@@ -6,34 +7,9 @@ const { encode, decode } = require('./index');
 const dictionary = require('src/dictionary');
 
 it('can encode & decode query', async () => {
-
-	const schemaObject = dictionary.buildSchema(`
-  input YellosArgs {
-    test: String
-  }
-  type Name {
-    text: String
-    url: String
-  }
-  type Entity {
-    id: Int
-    name: Name
-  }
-  type Query {
-    hello: Entity,
-    yello(arg1: Int arg2: String): String
-    hellos: [Entity]
-    yellos(arg2: YellosArgs arg3: String): [String]
-  }
-`);
-
-	const query = `
-{
-  hello { id }
-  hellos { id name { text url } }
-  yello(arg1: 1 arg2: "testasd")
-  yellos
-}`;
+	const schema = fs.readFileSync(`${__dirname}/test/artifacts/test1/schema.graphql`, 'utf8');
+	const query = fs.readFileSync(`${__dirname}/test/artifacts/test1/query.graphql`, 'utf8');
+	const schemaObject = dictionary.buildSchema(schema);
 
 
 	const parsedQuery = parse(query, {
@@ -51,3 +27,24 @@ it('can encode & decode query', async () => {
 	//Generated AST is valid. Query was x smaller in size
 	assert(compressionRate > 2.6);
 });
+
+// it('can encode & decode payload', async () => {
+// 	const schema = fs.readFileSync(`${__dirname}/test/artifacts/test1/schema.graphql`, 'utf8');
+// 	const query = fs.readFileSync(`${__dirname}/test/artifacts/test1/query.graphql`, 'utf8');
+// 	const schemaObject = dictionary.buildSchema(schema);
+//
+// 	const parsedQuery = parse(query, {
+// 		noLocation: true
+// 	});
+//
+// 	const schemaDictionary = await dictionary.generate(schemaObject);
+// 	const encoded = encode(parsedQuery, schemaDictionary);
+// 	const decoded = decode(encoded, schemaDictionary);
+//
+// 	assert.notStrictEqual(decoded[0], parsedQuery.definitions[0].selectionSet.selections);
+//
+// 	const compressionRate = (compress(print(parsedQuery)).length / encoded.length).toPrecision(3);
+//
+// 	//Generated AST is valid. Query was x smaller in size
+// 	assert(compressionRate > 2.6);
+// });
